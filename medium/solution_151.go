4594f8e60bdd44970c5669d3c4e7e3a29f099760
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-07-29 01:31:17 +0100

    solve medium soluton 141

--- medium/solution_151.go
+++ medium/solution_151.go
@@ -1,1 +1,20 @@
-package easy
+package medium
+
+import (
+	"slices"
+	"strings"
+)
+
+func ReverseWords(s string) string {
+	words := slices.DeleteFunc(strings.Split(s, " "), func(s string) bool {
+		return s == "" || s == " "
+	})
+	builder := strings.Builder{}
+	for i := len(words) - 1; i >= 0; i-- {
+		builder.WriteString(words[i])
+		if i != 0 {
+			builder.WriteString(" ")
+		}
+	}
+	return builder.String()
+}
